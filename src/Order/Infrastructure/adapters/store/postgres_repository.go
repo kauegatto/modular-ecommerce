04f6e4e133486d2acae9970d83ab1f5c1b0ebe24
Kaue Gatto <kauegatto123@gmail.com>
2025-02-15 00:36:50 -0300

    fix repo to fetch for status name

--- src/Order/Infrastructure/adapters/store/postgres_repository.go
+++ src/Order/Infrastructure/adapters/store/postgres_repository.go
@@ -36,10 +36,10 @@ func (repo PostgresRepository) Create(ctx context.Context, order *models.Order) 
 
 	_, err := repo.queries.CreateOrder(ctx, params)
 	if err != nil {
-		slog.ErrorContext(ctx, "Order when inserting order on db %w", err)
+		slog.ErrorContext(ctx, "Order when inserting order on db %w", slog.Attr{Key: "error", Value: slog.AnyValue(err)})
 		return err
 	}
-	slog.InfoContext(ctx, "Order inserted on db with success %w", order)
+	slog.InfoContext(ctx, "Order inserted on db with success", slog.Attr{Key: "order", Value: slog.AnyValue(order)})
 	return nil
 }
 
@@ -53,10 +53,10 @@ func (repo PostgresRepository) Update(ctx context.Context, order *models.Order) 
 
 	err := repo.queries.UpdateOrder(ctx, params)
 	if err != nil {
-		slog.ErrorContext(ctx, "Order when updating order on db %w", err)
+		slog.ErrorContext(ctx, "Order when updating order on db", slog.Attr{Key: "error", Value: slog.AnyValue(err)})
 		return err
 	}
-	slog.InfoContext(ctx, "Order updated on db with success %w", order)
+	slog.InfoContext(ctx, "Order updated on db with success %w", slog.Attr{Key: "order", Value: slog.AnyValue(order)})
 	return nil
 }
 
@@ -67,16 +67,22 @@ func (repo PostgresRepository) GetOrderById(ctx context.Context, id models.Order
 		return &models.Order{}, err
 	}
 
+	status, err := repo.queries.GetStatus(ctx, dbOrder.StatusID)
+	if err != nil {
+		slog.ErrorContext(ctx, "Could not find status", slog.Attr{Key: "StatusId", Value: slog.AnyValue(dbOrder.StatusID)})
+		return &models.Order{}, err
+	}
+
 	order := models.NewOrderFromDTO(
 		dbOrder.ID,
 		dbOrder.CustomerID,
-		models.OrderStatus(dbOrder.StatusID),
+		models.OrderStatus(status.StatusName),
 		dbOrder.CreatedAt.Time,
 		float64(dbOrder.TotalPrice),
 		int(dbOrder.Discount.Int.Int64()), // todo
 		[]models.OrderItem{},              // todo
 	)
 
-	slog.InfoContext(ctx, "Order found %w", order)
+	slog.InfoContext(ctx, "Order found %w", slog.Attr{Key: "OrderId", Value: slog.StringValue(id.String())})
 	return order, nil
 }
